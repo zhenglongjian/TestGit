2953b625c9abe2a120bdbfe731ae7d22efd173d7
zhenglongjian <906609471@qq.com>
2017-08-02 00:58:37 +0800

    我的第三次提交

--- src/main/java/com/atguigu/testgithubinecplise/MyCRUD.java
+++ src/main/java/com/atguigu/testgithubinecplise/MyCRUD.java
@@ -6,6 +6,7 @@ public class MyCRUD {
     public static void main(String[] args) {
         System.out.println("哈哈我是中国人");
         System.out.println("我为我自己感到自豪");
+        System.out.println("我讨厌日本人");
     }
     
 }
